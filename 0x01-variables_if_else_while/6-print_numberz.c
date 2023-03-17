3df1c1af0a28a05c243526dbac2db51d27bfa2bc
IreneMercy <irenemercy700@gmail.com>
2023-03-17 16:58:43 -0700

    print single numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'print single numbers of base 10'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
