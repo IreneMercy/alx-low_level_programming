4a135e7a6ffb48f0cd2193cb343e89f96cc1890e
IreneMercy <irenemercy700@gmail.com>
2023-03-21 14:29:14 -0700

    print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+	int count;
+
+	while (count++ <= 9)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+			_putchar(x);
+		_putchar('\n');
+	}
+}
