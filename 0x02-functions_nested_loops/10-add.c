a160cbf3def1fe77aab55baf1a077d4cf2ca230b
IreneMercy <irenemercy700@gmail.com>
2023-03-21 16:53:24 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n, m, sum = 0;
+
+	sum = n + m;
+	printf("%d\n", sum);
+	return (0);
+}
