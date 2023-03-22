fd3a5f0c011bafdc147fd62028b50190017ba5f6
IreneMercy <irenemercy700@gmail.com>
2023-03-21 17:21:33 -0700

    sum

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * add - check the code
+ *
  * @n: sum
  *
  * @m: sum
  *
  * Return: Always 0.
  */
-int main(int n, int m)
+int add(int n, int m)
 {
 	return (n + m);
 }
