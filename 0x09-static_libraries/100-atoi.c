e07102a4f41961709660744436dc7159ccb0dc86
IreneMercy <irenemercy700@gmail.com>
2023-04-10 07:24:03 -0700

    script that creates a static library

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,5 +1,23 @@
 #include "main.h"
 
+/**
+ * _atoi - string to integer
+ * @s: string value
+ * Return: 0 success
+ */
+
 int _atoi(char *s)
 {
+	int x = 1;
+	int y = 0;
+	unsigned int rt = 0;
+
+	while(s[y] <= 9 && s[y] >= 0 && s[y] != '\0')
+	{
+		rt = (rt * 10) + (s[y] - '0');
+		y++;
+
+	}
+	rt = x;
+	return (rt);
 }
