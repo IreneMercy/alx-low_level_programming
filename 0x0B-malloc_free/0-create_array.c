aa47424e25202b7e7d4b512f3df1bbfa1b186006
IreneMercy <irenemercy700@gmail.com>
2023-04-11 11:55:09 -0700

    create an array of characters

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *_strdup - function that creates an array of chars
+ * @size: size of array
+ * @c: character value
+ * Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int x;
+
+	ptr = malloc(sizeof(char) * size);
+	if (ptr == NULL || size == 0)
+		return (NULL);
+	for (x = 0; x < size; x++)
+		ptr[x] = c;
+	return (ptr);
+
+}
