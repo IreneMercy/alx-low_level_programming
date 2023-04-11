a4f00e48d1d12a62a1fd426f3864026b3d635314
IreneMercy <irenemercy700@gmail.com>
2023-04-11 13:49:08 -0700

    frees a 2 dimensional grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -6,7 +6,6 @@
  * **alloc_grid -  returns a pointer to a 2 dimensional array of integers
  * @width: width of the grid
  * @height: height of the grid
- *
  * Return: Nothing.
  */
 int **alloc_grid(int width, int height)
