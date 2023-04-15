deef6a8c49da8dfa5431f8081914636d732bd021
IreneMercy <irenemercy700@gmail.com>
2023-04-15 15:39:51 -0700

    array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,7 +1,22 @@
 #include <stdlib.h>
 /**
- * *_calloc - allocates memory for an array, using malloc
- * @nmemb: array of nmemb elements
- * @size: size of bytes
+ * *array_range - creates an array of integers
+ * @max: highest amount of value
+ * @min: lowest amount of value
  * Return: 0
  */
+int *array_range(int min, int max)
+{
+	int *ptr, len, item;
+
+	if (min > max)
+		return (NULL);
+	len = max - min + 1;
+
+	ptr = malloc(sizeof(int) * len);
+	if (ptr == NULL)
+		return (NULL);
+	for (item = 0; min <= max; item++)
+		ptr[item] = min++;
+	return (ptr);
+}
