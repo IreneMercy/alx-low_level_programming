647402d725af71899e3ed5848bb96e89c4b6fbad
IreneMercy <irenemercy700@gmail.com>
2023-04-15 15:14:34 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
+int strlength(char *str);
 /**
  * *string_nconcat - concatenate strings
  * @s1: string value
@@ -11,32 +12,36 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0;
-	unsigned int j = 0;
-	unsigned int s1_leng = 0;
-	unsigned int s2_leng = 0;
-	char *str;
+	unsigned int length = 0;
+	unsigned int item_1;
+	unsigned int item_2;
+	unsigned int len2;
+	char *ptr;
 
-	while (s1 && s1[s1_leng])
-		s1_leng++;
-	while (s2 && s2[s2_leng])
-		s2_leng++;
-	if (n >= s2_leng)
-		str = malloc(sizeof(char) * (s1_leng + n + 1));
-	else
-		str = malloc(sizeof(char) * (s1_leng + s2_leng + 1));
-	if (str == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (item_1 = 0; s1[item_1]; item_1++)
+		length++;
+
+	len2 = length + n;
+
+	ptr = malloc(sizeof(char) * (len2 + 1));
+	if (ptr == NULL)
 		return (NULL);
-	while (i < s1_leng)
+
+	for (item_1 = 0; item_1 < length; item_1++)
 	{
-		str[i] = s1[i];
-		i++;
+		ptr[item_1] = s1[item_1];
 	}
-	while (n < s2_leng && i < (s1_leng + n))
-		str[i++] = s2[j++];
-	while (n >= s2_leng && i < (s1_leng + s2_leng))
-		str[i++] = s2[j++];
-	str[i] = '\0';
-	return (str);
-	free(str);
+
+	for (item_2 = 0; item_2 < n; item_2++, item_1++)
+	{
+		ptr[item_1] = s2[item_2];
+	}
+
+	ptr[item_1] = '\0';
+
+	return (ptr);
 }
