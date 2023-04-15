630afc432035ea8f8b40776c23e4342cf19940f7
IreneMercy <irenemercy700@gmail.com>
2023-04-15 11:17:42 -0700

    memory allocation

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 
 	y = nmemb * size;
-	ptr = malloc(sizeof(char) * y);
+	ptr = malloc(y);
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
