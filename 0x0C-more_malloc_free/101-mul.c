887d339ceaca68732793425c157e4b56f25a9a7d
IreneMercy <irenemercy700@gmail.com>
2023-04-15 18:14:21 -0700

    multiply positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,35 +1,98 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#define ERR_MSG "Error"
 /**
- * main - multiplies positive numbers
- * @argc: number of arguments
+ * digit - checks if a string contains only digits
+ * @ptr: string value
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int digit(char *ptr)
+{
+	int x = 0;
+
+	while (ptr[x])
+	{
+		if (ptr[x] < '0' || ptr[x] > '9')
+			return (0);
+		x++;
+	}
+	return (1);
+}
+
+
+/**
+ * strlength - finds the length of a string
+ * @ptr: string value
+ * Return: 0
+ */
+int strlength(char *ptr)
+{
+	int x = 0;
+
+	while (ptr[x] != '\0')
+	{
+		x++;
+	}
+
+	return (x);
+}
+
+/**
+ * errors - displays errors found in main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments entered
  * @argv: string values
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int x, y;
+	char *s_1, *s_2;
+	int len_1, len_2, len, k, conta, item_1, item_2, *final, z = 0;
 
-	if (argc != 3)
+	s_1 = argv[1], s_2 = argv[2];
+	if (argc != 3 || !digit(s_1) || !digit(s_2))
+		errors();
+	len_1 = strlength(s_1);
+	len_2 = strlength(s_2);
+	len = len_1 + len_2 + 1;
+	final = malloc(sizeof(int) * len);
+	if (!final)
+		return (1);
+	for (k = 0; k <= len_1 + len_2; k++)
+		final[k] = 0;
+	for (len_1 = len_1 - 1; len_1 >= 0; len_1--)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (x = 1; x < argc; x++)
-	{
-		for (y = 0; argv[x][y] != '\0'; y++)
+		item_1 = s_1[len_1] - '0';
+		conta = 0;
+		for (len_2 = strlength(s_2) - 1; len_2 >= 0; len_2--)
 		{
-			if (argv[x][y] > 57 || argv[x][y] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			item_2 = s_2[len_2] - '0';
+			conta += final[len_1 + len_2 + 1] + (item_1 * item_2);
+			final[len_1 + len_2 + 1] = conta % 10;
+			conta /= 10;
 		}
+		if (conta > 0)
+			final[len_1 + len_2 + 1] += conta;
 	}
-	mul = atoi(argv[1]) * atoi(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+	for (k = 0; k < len - 1; k++)
+	{
+		if (final[k])
+			z = 1;
+		if (z)
+			_putchar(final[k] + '0');
+	}
+	if (!z)
+		_putchar('0');
+	_putchar('\n');
+	free(final);
+	return (0);
 }
-
