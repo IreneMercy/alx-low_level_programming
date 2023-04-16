2bad26ebb9d292c295ddc351aee754f64a78462d
IreneMercy <irenemercy700@gmail.com>
2023-04-15 17:00:09 -0700

    multiply

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - multiplies positive numbers
+ * @argc: number of arguments
+ * @argv: string values
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	unsigned long mul;
+	int x, y;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (argv[x][y] > 57 || argv[x][y] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%lu\n", mul);
+return (0);
+}
+
