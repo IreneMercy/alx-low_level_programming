31befd67650eab97ad6df2fee1245ba6390e9984
IreneMercy <irenemercy700@gmail.com>
2023-04-17 14:00:09 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,5 +16,8 @@ void print_dog(struct dog *d)
 	if (d->owner == NULL)
 		printf("Owner:(nil)\n");
 
+	if (d->age < 0)
+		printf("Age:(nil)\n");
+
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
