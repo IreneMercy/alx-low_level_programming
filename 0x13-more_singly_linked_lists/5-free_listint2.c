9ab8252f6275d72cf2a6de05245bae3e3e0e5d44
IreneMercy <irenemercy700@gmail.com>
2023-05-01 09:32:42 -0700

    fixing error on setting head to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,11 +12,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *final_node;
 
-	if (*head == NULL || head == NULL)
+	if ((*head) == NULL && head == NULL)
 	{
 		return;
 	}
-	while (*head != NULL)
+	while ((*head) != NULL)
 	{
 		final_node = *head;
 		*head = (*head)->next;
