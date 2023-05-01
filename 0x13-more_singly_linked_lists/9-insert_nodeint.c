bbc7a4fcac18afa5f599bcc10481c52247186f47
IreneMercy <irenemercy700@gmail.com>
2023-05-01 14:25:29 -0700

    insert a new node at a set index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	listint_t *new_node, *newest_node;
+	listint_t *new_node = NULL, *newest_node = NULL;
 
 	if (head == NULL || *head == NULL)
 	{
