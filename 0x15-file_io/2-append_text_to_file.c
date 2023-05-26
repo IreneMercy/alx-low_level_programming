082f68e9ca8b04efd010deb6d0737a6a28effc00
IreneMercy <irenemercy700@gmail.com>
2023-05-26 13:25:04 -0700

    2-append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,24 +8,27 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int len, written_bytes, actions;
+	int len, written, fd;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
 	{
 		for (len = 0; text_content[len];)
 			len++;
+		written = write(fd, text_content, len);
+
+		if (written == -1)
+			return (-1);
 	}
 
-	actions = open(filename, O_WRONLY | O_APPEND);
-	written_bytes = write(actions, text_content, len);
-
-	if (actions == -1 || written_bytes == -1)
-		return (-1);
-
-	close(actions);
+	close(fd);
 
 	return (1);
 }
