489f82bb38dcbe9038c26226b326c6515e83248c
IreneMercy <irenemercy700@gmail.com>
2023-05-26 14:35:00 -0700

    3-cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,12 +2,17 @@
 #define IOFILE_H
 
 #include <stddef.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-size_t _strnlen(const char *str, size_t maxlen);
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
 #endif
